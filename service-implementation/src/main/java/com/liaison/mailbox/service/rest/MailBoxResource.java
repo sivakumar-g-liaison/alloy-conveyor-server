422b3e216a205ab879ecaec3110a9e9c6b2c48ca
veerasamy.nagarajan <veerasamy.nagarajan@objectfrontier.com>
2013-08-23 20:16:57 +0530

    Created a post rest service to insert the config data to the mailbox component.

--- service-implementation/src/main/java/com/liaison/mailbox/service/rest/MailBoxResource.java
+++ service-implementation/src/main/java/com/liaison/mailbox/service/rest/MailBoxResource.java
@@ -12,15 +12,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.liaison.mailbox.service.core.MailBox;
-import com.liaison.mailbox.service.core.component.tasks.DirectorySweeperTaskHandler;
 
 /**
  *
@@ -58,4 +57,29 @@ public class MailBoxResource {
 	return null;
     }
 
+	/**
+	 * REST Method to insert the analytic profile details.
+	 *
+	 * @return JSON meta data of file groups
+	 */
+	@Path("/addConfig")
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response addConfig(@QueryParam("profile") String profile,
+			@QueryParam("name") String name,
+			@QueryParam("url") String url,
+			@QueryParam("id") String id) {
+		//TODO change to json input.
+		try {
+
+			MailBox mailBox = new MailBox();
+			mailBox.insertProfileComponents(name, profile, url, id);
+			LOGGER.info("Configuration added successfully for profile ::{}", profile);
+		} catch (Exception e) {
+			LOGGER.error("Error in directory sweeping.", e);
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+					.build();
+		}
+		return null;
+	}
 }
