14011aa1383d069955d83357e171b3d89098c6de
unknown <karthikeyanm@PC1366.itp.objectfrontier.com>
2013-09-04 13:15:19 +0530

    Revise processor implementation

---
+++ service-implementation/src/main/java/com/liaison/mailbox/service/dto/configuration/response/ReviseProcessorResponseDTO.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright Liaison Technologies, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Liaison Technologies, Inc. ("Confidential Information").  You shall 
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Liaison Technologies.
+ */
+package com.liaison.mailbox.service.dto.configuration.response;
+
+import com.liaison.mailbox.service.dto.ResponseDTO;
+import com.liaison.mailbox.service.dto.configuration.ProcessorDTO;
+
+/**
+ * @author karthikeyanm
+ *
+ */
+public class ReviseProcessorResponseDTO {
+
+	private ResponseDTO response;
+	private ProcessorDTO processor;
+	
+	/**
+	 * @return the response
+	 */
+	public ResponseDTO getResponse() {
+		return response;
+	}
+	/**
+	 * @param response the response to set
+	 */
+	public void setResponse(ResponseDTO response) {
+		this.response = response;
+	}
+	/**
+	 * @return the processor
+	 */
+	public ProcessorDTO getProcessor() {
+		return processor;
+	}
+	/**
+	 * @param processor the processor to set
+	 */
+	public void setProcessor(ProcessorDTO processor) {
+		this.processor = processor;
+	}
+	
+	
+}
