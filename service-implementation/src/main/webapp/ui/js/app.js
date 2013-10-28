6df3b61be1bf9037e2d52314205bef7d78243eba
veerasamy.nagarajan <veerasamy.nagarajan@objectfrontier.com>
2013-10-28 12:34:51 +0530

    changed to relative URL

--- service-implementation/src/main/webapp/ui/js/app.js
+++ service-implementation/src/main/webapp/ui/js/app.js
@@ -103,9 +103,9 @@ myApp.run(function ($rootScope, $location, $http, $timeout, AuthService, RESTSer
     // Eager load some data using simple REST client
     // *****
 
-    // $rootScope.base_url = '../g2mailboxservice/rest/v1/mailbox';
+    $rootScope.base_url = '../g2mailboxservice/rest/v1/mailbox';
     //FOR USE WITH PYTHON
-    $rootScope.base_url = 'http://localhost:8080/g2mailboxservice/rest/v1/mailbox';
+    //$rootScope.base_url = 'http://localhost:8080/g2mailboxservice/rest/v1/mailbox';
     $rootScope.restService = RESTService;
     $rootScope.informer = Informer;
 
