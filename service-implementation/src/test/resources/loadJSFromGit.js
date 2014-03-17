4fbdb0e82cd98f55c136053f2c25b79bbca3a803
santoshc_ofs <santhoshkumar.chandran@object-frontier.com>
2014-03-17 20:38:42 +0530

    temp js file updated for loading js from git

--- service-implementation/src/test/resources/loadJSFromGit.js
+++ service-implementation/src/test/resources/loadJSFromGit.js
@@ -1,3 +1,35 @@
 function init(processor, logger) {
-	logger.info('js called from git...');
+    logger.info("Javascript executed via custom JS engine");
+    var javaImports = new JavaImporter(
+		Packages.com.google.api.client.json.jackson2,
+        Packages.org.apache.commons.codec.binary,
+		Packages.org.codehaus.jackson.map);
+	with(javaImports) {
+        logger.info("Displaying List of Remote Processor Properties");
+        var remoteProcessorProperties = processor.getRemoteProcessorProperties();
+        logger.info("url " +remoteProcessorProperties.getUrl());
+        logger.info("HTTP Version " +remoteProcessorProperties.getHttpVersion());
+        logger.info("HTTP Verb " +remoteProcessorProperties.getHttpVerb());
+        logger.info("Retry Attempts " +remoteProcessorProperties.getRetryAttempts());
+        logger.info("Socket Timeout " +remoteProcessorProperties.getSocketTimeout());
+        logger.info("Connection Timeout " +remoteProcessorProperties.getConnectionTimeout());
+        logger.info("Chunked Encoding " +remoteProcessorProperties.isChunkedEncoding());
+        logger.info("Content Type " +remoteProcessorProperties.getContentType());
+        logger.info("Encoding Format " +remoteProcessorProperties.getEncodingFormat());
+        logger.info("Passive " +remoteProcessorProperties.isPassive());
+        logger.info("Binary " +remoteProcessorProperties.isBinary());
+        logger.info("Retry Interval " +remoteProcessorProperties.getRetryInterval());
+        logger.info("Pipeline ID " +remoteProcessorProperties.getPipeLineID());
+        logger.info("Displaying Response location" + processor.getWriteResponseURI());
+        logger.info("OtherRequest Header " + remoteProcessorProperties.getOtherRequestHeader());
+        
+        logger.info("Displaying List of Dynamic Processor Properties");
+        var properties = processor.getDynamicProperties();
+        logger.info("Processed File Location "+ properties.getProperty("processedfilelocation"));
+        logger.info("File Rename Format "+ properties.getProperty("filerenameformat"));
+        logger.info("Sweeped File Location "+ properties.getProperty("sweepedfilelocation"));
+        logger.info("Error File Location "+ properties.getProperty("errorfilelocation"));
+              
+     }
+    
 }
