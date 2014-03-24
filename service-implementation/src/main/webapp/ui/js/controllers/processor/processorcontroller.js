14201d40ad8a055e9bb88e2d769ddd6e791ec5fe
unknown <roshans@PC1483.itp.objectfrontier.com>
2014-03-24 19:31:32 +0530

    JIRA issue fixed for GMB-155

--- service-implementation/src/main/webapp/ui/js/controllers/processor/processorcontroller.js
+++ service-implementation/src/main/webapp/ui/js/controllers/processor/processorcontroller.js
@@ -2391,7 +2391,7 @@ var rest = myApp.controller(
 				var basePort = baseUrl.split('/')[2].split(':')[1]
 				if (basePort === '' || basePort === null || typeof basePort === 'undefined') {
 					var defaultPort = $scope.processor.remoteProcessorProperties.port;
-					if (defaultPort != '' || defaultPort != null || typeof defaultPort != 'undefined') {
+					if (defaultPort !== '') {
 						var url_parts = baseUrl.split('/');
 						var domain_name_parts = url_parts[2].split(':');
 						var domainWithPort = domain_name_parts[0].concat(':', defaultPort);
