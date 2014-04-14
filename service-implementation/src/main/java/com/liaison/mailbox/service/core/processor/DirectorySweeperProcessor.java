f6ee89da1c270679d02478cea83127478921539d
Veerasamy <vnagarajan@liaison.com>
2014-04-14 16:03:21 -0400

    Directory sweeper last modified tolerance implementation.

--- service-implementation/src/main/java/com/liaison/mailbox/service/core/processor/DirectorySweeperProcessor.java
+++ service-implementation/src/main/java/com/liaison/mailbox/service/core/processor/DirectorySweeperProcessor.java
@@ -69,6 +69,7 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 	private static final Logger LOGGER = LogManager.getLogger(DirectorySweeperProcessor.class);
 
 	private String pipeLineID;
+	private List<Path> inProgressFiles = new ArrayList<>();
 
 	public void setPipeLineID(String pipeLineID) {
 		this.pipeLineID = pipeLineID;
@@ -85,13 +86,13 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 
 	@Override
 	public void invoke(String executionId,MailboxFSM fsm) throws Exception {
-	
-		if (!MailBoxUtility.isEmpty(configurationInstance.getJavaScriptUri())) {	
+
+		if (!MailBoxUtility.isEmpty(configurationInstance.getJavaScriptUri())) {
 			fsm.handleEvent(fsm.createEvent(ExecutionEvents.PROCESSOR_EXECUTION_HANDED_OVER_TO_JS));
 			// Use custom G2JavascriptEngine
 			JavaScriptEngineUtil.executeJavaScript(configurationInstance.getJavaScriptUri(), "init", this,LOGGER);
 		} else {
-			executeRequest();	
+			executeRequest();
 		}
 	}
 
@@ -100,30 +101,31 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 		// Get root from folders input_folder
 		String inputLocation = getPayloadURI();
 		String fileRenameFormat = getDynamicProperties().getProperty(MailBoxConstants.FILE_RENAME_FORMAT_PROP_NAME);
+		fileRenameFormat = (fileRenameFormat == null) ? MailBoxConstants.SWEEPED_FILE_EXTN : fileRenameFormat;
+
+		long timeLimit = MailBoxUtility.getEnvironmentProperties().getLong(MailBoxConstants.LAST_MODIFIED_TOLERANCE);
 
 		// Validation of the necessary properties
 		if (MailBoxUtility.isEmpty(inputLocation)) {
 			throw new MailBoxServicesException(Messages.PAYLOAD_LOCATION_NOT_CONFIGURED);
 		}
-		if (null == fileRenameFormat) {
-			fileRenameFormat = MailBoxConstants.SWEEPED_FILE_EXTN;
-		}
 
-		// Sweeps the directory and constructs the list of file attributes dto.
-		List<FileAttributesDTO> files = sweepDirectory(inputLocation, false, false, null, fileRenameFormat);
+		List<FileAttributesDTO> files = (inProgressFiles.isEmpty())
+				? sweepDirectory(inputLocation, false, false, null, fileRenameFormat, timeLimit)
+				: validateInprogressFiles(inProgressFiles, timeLimit);
 
 		if (files.isEmpty()) {
-			LOGGER.info("The given directory is empty.");
+			LOGGER.info("There are no files to process.");
 		} else {
 
 			// Read from mailbox property - grouping js location
 			List<List<FileAttributesDTO>> fileGroups = groupingFiles(files);
 
-			String sweepedFileLocation = processMountLocation(getDynamicProperties().getProperty(MailBoxConstants.SWEEPED_FILE_LOCATION));
+			String sweepedFileLocation = processMountLocation(getDynamicProperties().getProperty(
+					MailBoxConstants.SWEEPED_FILE_LOCATION));
 			if (!MailBoxUtility.isEmpty(sweepedFileLocation)) {
 
-				// If the given sweeped file location is not available then
-				// system will create that.
+				// If the given sweeped file location is not available then system will create that.
 				Path path = Paths.get(sweepedFileLocation);
 				if (!Files.isDirectory(path)) {
 					Files.createDirectories(path);
@@ -146,7 +148,13 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 
 			}
 		}
+
+		// call again when in-progress file list is not empty
+		if (!inProgressFiles.isEmpty()) {
+			executeRequest();
+		}
 	}
+
 
 	/**
 	 * Method is used to retrieve all the files attributes from the given mailbox. This method
@@ -162,10 +170,10 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 	 * @throws URISyntaxException
 	 * @throws MailBoxServicesException
 	 * @throws FS2Exception
-	 * @throws JAXBException 
+	 * @throws JAXBException
 	 */
 	public List<FileAttributesDTO> sweepDirectory(String root, boolean includeSubDir, boolean listDirectoryOnly,
-			SweepConditions sweepConditions, String fileRenameFormat) throws IOException, URISyntaxException,
+			SweepConditions sweepConditions, String fileRenameFormat, long timeLimit) throws IOException, URISyntaxException,
 			MailBoxServicesException, FS2Exception, JAXBException {
 
 		Path rootPath = Paths.get(root);
@@ -178,6 +186,12 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 			for (Path file : stream) {
 
 				if (!file.getFileName().toString().contains(fileRenameFormat)) {
+
+					if (validateLastModifiedTolerance(timeLimit, file)) {
+						LOGGER.info("The file {} is in progress. So added in the in-progress list.", file.getFileName());
+						inProgressFiles.add(file);
+						continue;
+					}
 					result.add(file);
 				}
 			}
@@ -185,22 +199,7 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 			throw e;
 		}
 
-		List<FileAttributesDTO> fileAttributes = new ArrayList<>();
-		FileAttributesDTO attribute = null;
-		for (Path path : result) {
-
-			attribute = new FileAttributesDTO();
-			attribute.setFs2Path(path.toAbsolutePath().toString());
-			attribute.setPipeLineID(getPipeLineID());
-
-			BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
-			attribute.setTimestamp(attr.creationTime().toString());
-			attribute.setSize(attr.size());
-			attribute.setFilename(path.toFile().getName());
-			fileAttributes.add(attribute);
-		}
-
-		return fileAttributes;
+		return getFileAttributes(result);
 	}
 
 	/**
@@ -212,11 +211,10 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 	 * @throws JsonMappingException
 	 * @throws IOException
 	 */
-	private String getPipeLineID() throws JAXBException,
-			JsonParseException, JsonMappingException, IOException {
-		
-		if(MailBoxUtility.isEmpty(this.pipeLineID)) {
-			
+	private String getPipeLineID() throws JAXBException, JsonParseException, JsonMappingException, IOException {
+
+		if (MailBoxUtility.isEmpty(this.pipeLineID)) {
+
 			RemoteProcessorPropertiesDTO properties = MailBoxUtility.unmarshalFromJSON(configurationInstance.getProcsrProperties(),
 					RemoteProcessorPropertiesDTO.class);
 			this.setPipeLineID(properties.getPipeLineID());
@@ -238,24 +236,15 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 	 * @throws MailBoxServicesException
 	 */
 	private List<List<FileAttributesDTO>> groupingFiles(List<FileAttributesDTO> files) throws ScriptException,
-			IOException, URISyntaxException, NoSuchMethodException, MailBoxServicesException, Exception {
+	IOException, URISyntaxException, NoSuchMethodException, MailBoxServicesException, Exception {
 
 		String groupingJsPath = configurationInstance.getJavaScriptUri();
 		List<List<FileAttributesDTO>> fileGroups = new ArrayList<>();
 
 		if (!MailBoxUtility.isEmpty(groupingJsPath)) {
 
-			/*ScriptEngineManager manager = new ScriptEngineManager();
-			ScriptEngine engine = manager.getEngineByName("JavaScript");
-
-			engine.eval(getJavaScriptString(groupingJsPath));
-			Invocable inv = (Invocable) engine;
-
-			// invoke the method in javascript
-			inv.invokeFunction("init", files);*/
-			
 			// Use custom G2JavascriptEngine
-			JavaScriptEngineUtil.executeJavaScript(groupingJsPath, "init", files,LOGGER);
+			JavaScriptEngineUtil.executeJavaScript(groupingJsPath, "init", files, LOGGER);
 
 		} else {
 
@@ -329,71 +318,50 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 	public void markAsSweeped(List<FileAttributesDTO> fileList, String fileRenameFormat, String sweepedFileLocation)
 			throws IOException, JSONException, FS2Exception, URISyntaxException {
 
-		if (MailBoxUtility.isEmpty(sweepedFileLocation)) {
+		Path target = null;
+		Path oldPath = null;
+		Path newPath = null;
+		FlexibleStorageSystem FS2 = null;
+		FS2MetaSnapshot metaSnapShot = null;
 
-			LOGGER.info("Renaming the processed files");
-			for (FileAttributesDTO file : fileList) {
-
-				Path oldPath = new File(file.getFs2Path()).toPath();
-				Path newPath = oldPath.getParent().resolve(oldPath.toFile().getName() + fileRenameFormat);
-
-				// Creating meta snapshot
-				FlexibleStorageSystem FS2 = FS2InstanceCreator.getFS2Instance();
-				FS2MetaSnapshot metaSnapShot = new FS2MetaSnapshotImpl(newPath.toUri(),true, new Date(), "DirectorySweeper",new FS2ObjectHeaders());
-
-				// Constructing the fs2 file
-				try {
-					FS2.createObjectEntry(oldPath.toUri(), metaSnapShot.toJSON(), null);
-
-				} catch (FS2ObjectAlreadyExistsException e) {
-					FS2.deleteRecursive(oldPath.toUri());
-					FS2.createObjectEntry(oldPath.toUri(), metaSnapShot.toJSON(), null);
-				} 
-
-				// Renaming the file at the end of the step when everything is
-				// done.
-				move(oldPath, newPath);
-				file.setFs2Path(CoreFS2Utils.genURIFromPath(oldPath.toFile().getAbsolutePath()).toString());
-				file.setGuid(MailBoxUtility.getGUID());
-			}
-
-		} else {
-
-			LOGGER.info("Renaming the processed files with sweeped file location.");
-
-			Path target = Paths.get(sweepedFileLocation);
-			for (FileAttributesDTO file : fileList) {
-
-				Path oldPath = new File(file.getFs2Path()).toPath();
-				Path newPath = target.resolve(oldPath.toFile().getName() + fileRenameFormat);
-
-				// Creating meta snapshot
-				FlexibleStorageSystem FS2 = FS2InstanceCreator.getFS2Instance();
-				FS2MetaSnapshot metaSnapShot = new FS2MetaSnapshotImpl(newPath.toUri(),true, new Date(), "DirectorySweeper",new FS2ObjectHeaders());
-
-				// Constructing the fs2 file
-				try {
-					FS2.createObjectEntry(oldPath.toUri(), metaSnapShot.toJSON(), null);
-
-				} catch (FS2ObjectAlreadyExistsException e) {
-					FS2.deleteRecursive(oldPath.toUri());
-					FS2.createObjectEntry(oldPath.toUri(), metaSnapShot.toJSON(), null);
-				}
-
-				// Renaming the file at the end of the step when everything is
-				// done.
-				move(oldPath, newPath);
-				file.setFs2Path(CoreFS2Utils.genURIFromPath(oldPath.toFile().getAbsolutePath()).toString());
-				file.setGuid(MailBoxUtility.getGUID());
-			}
+		if (!MailBoxUtility.isEmpty(sweepedFileLocation)) {
+			target = Paths.get(sweepedFileLocation);
 		}
+
+		LOGGER.info("Renaming the processed files");
+		for (FileAttributesDTO file : fileList) {
+
+			oldPath = new File(file.getFs2Path()).toPath();
+			newPath = (target == null) ? oldPath.getParent().resolve(oldPath.toFile().getName() + fileRenameFormat)
+					: target.resolve(oldPath.toFile().getName() + fileRenameFormat);
+
+			// Creating meta snapshot
+			FS2 = FS2InstanceCreator.getFS2Instance();
+			metaSnapShot = new FS2MetaSnapshotImpl(CoreFS2Utils.genURIFromPath(newPath.toString()), true, new Date(),
+					"DirectorySweeper", new FS2ObjectHeaders());
+
+			// Constructing the fs2 file
+			try {
+				FS2.createObjectEntry(CoreFS2Utils.genURIFromPath(oldPath.toString()), metaSnapShot.toJSON(), null);
+
+			} catch (FS2ObjectAlreadyExistsException e) {
+				FS2.deleteRecursive(oldPath.toUri());
+				FS2.createObjectEntry(oldPath.toUri(), metaSnapShot.toJSON(), null);
+			}
+
+			// Renaming the file at the end of the step when everything is done.
+			move(oldPath, newPath);
+			file.setFs2Path(CoreFS2Utils.genURIFromPath(oldPath.toFile().getAbsolutePath()).toString());
+			file.setGuid(MailBoxUtility.getGUID());
+		}
+
 		LOGGER.info("Renaming the processed files - done");
 
 	}
 
 	/**
 	 * Method is used to move the file to the sweeped folder.
-	 * 
+	 *
 	 * @param file
 	 *            The source location
 	 * @param target
@@ -406,7 +374,7 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 
 	/**
 	 * Method is used to construct the MetaData JSON from the file attributes dto list.
-	 * 
+	 *
 	 * @param files
 	 *            The file attributes list
 	 * @return String MetaData JSON string
@@ -416,13 +384,94 @@ public class DirectorySweeperProcessor extends AbstractRemoteProcessor implement
 	 * @throws JAXBException
 	 */
 	private String constructMetaDataJson(List<FileAttributesDTO> files) throws JsonGenerationException,
-			JsonMappingException, IOException, JAXBException {
+	JsonMappingException, IOException, JAXBException {
 
 		FileGroupDTO group = new FileGroupDTO();
 		group.getFileAttributes().addAll(files);
 
 		String jsonResponse = MailBoxUtility.marshalToJSON(group);
 		return jsonResponse;
+	}
+
+	/**
+	 * Returns file attributes from java.io.File
+	 *
+	 * @param result
+	 *            files
+	 * @return list of file attributes
+	 * @throws JAXBException
+	 * @throws JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
+	private List<FileAttributesDTO> getFileAttributes(List<Path> result) throws JAXBException, IOException {
+
+		List<FileAttributesDTO> fileAttributes = new ArrayList<>();
+		FileAttributesDTO attribute = null;
+		BasicFileAttributes attr = null;
+		for (Path path : result) {
+
+			attribute = new FileAttributesDTO();
+			attribute.setFs2Path(path.toAbsolutePath().toString());
+			attribute.setPipeLineID(getPipeLineID());
+
+			attr = Files.readAttributes(path, BasicFileAttributes.class);
+			attribute.setTimestamp(attr.creationTime().toString());
+			attribute.setSize(attr.size());
+			attribute.setFilename(path.toFile().getName());
+			fileAttributes.add(attribute);
+		}
+		return fileAttributes;
+	}
+
+	/**
+	 * Checks whether a file is modified with in the given time limit
+	 * 
+	 * @param timelimit
+	 *            to check the file is modified with in the given time limit
+	 * @param file
+	 *            File object
+	 * @return true if it is updated with in the given time limit, false otherwise
+	 */
+	public boolean validateLastModifiedTolerance(long timelimit, Path file) {
+
+		long system = System.currentTimeMillis();
+		long lastmo = file.toFile().lastModified();
+
+		if (((system - lastmo)/1000) < timelimit) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Checks recently in-progress files are completed or not
+	 * 
+	 * @param inprogressFiles
+	 *            list of recently update files
+	 * @param timelimit
+	 *            period of the recent modifications
+	 * @return list of file attribute dto
+	 * @throws JAXBException
+	 * @throws IOException
+	 */
+	private List<FileAttributesDTO> validateInprogressFiles(List<Path> inprogressFiles, long timelimit)
+			throws JAXBException, IOException {
+
+		List<Path> files = new ArrayList<>();
+		for (Path file : inprogressFiles) {
+
+			if (!validateLastModifiedTolerance(timelimit, file)) {
+				LOGGER.info("There are no changes in the file {} recently. So it is added in the sweeper list.", file.getFileName());
+				files.add(file);
+				continue;
+			}
+			LOGGER.info("The file {} is still in progress. So it is removed from the in-progress list.", file.getFileName());
+		}
+
+		inprogressFiles.clear();//Clearing the recently updated files after the second call.
+		return getFileAttributes(files);
 	}
 
 }
