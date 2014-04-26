7eb7a1c1b3618a58dd75ead71c10dd57f65c3aee
santoshc_ofs <santhoshkumar.chandran@object-frontier.com>
2014-04-26 23:18:18 +0530

    jquery framework used to base64 encode and decode in js

--- service-implementation/src/main/webapp/ui/js/controllers/processor/processorcontroller.js
+++ service-implementation/src/main/webapp/ui/js/controllers/processor/processorcontroller.js
@@ -1463,7 +1463,7 @@ var rest = myApp.controller(
 				$scope.restService.get(url,
 					function (secretData, status) {
 						if(status === 200) {
-							var decPwd = decode64(decode64(secretData));
+							var decPwd = $.base64.decode($.base64.decode(secretData));
 							data.getProcessorResponse.processor.credentials[a].password = decPwd;
 							$scope.editProcAfterReadSecret(data, profData, procsrId, blockuiFlag);
 						} else if(status === 404) {
@@ -1988,7 +1988,7 @@ var rest = myApp.controller(
 						$scope.secretName = '';
 						if($scope.credType === 'LOGIN_CREDENTIAL') {
 							$scope.secretName = $scope.mailboxName + $scope.procName + $scope.credUsrName;
-							base64EncodedSecret = $scope.base64EncodedSecret = encode64($scope.secret);
+							base64EncodedSecret = $scope.base64EncodedSecret = $.base64.encode($scope.secret);
 							$scope.secretUrl = $scope.url_secret_service + encodeURIComponent($scope.secretName);
 							reviseSecret($scope.secretUrl, base64EncodedSecret, i);
 							break;
@@ -2017,7 +2017,7 @@ var rest = myApp.controller(
 						if($scope.credType === 'LOGIN_CREDENTIAL') {
 						
 							$scope.secretName = $scope.mailboxName + $scope.procName + $scope.credUsrName;
-							base64EncodedSecret = $scope.base64EncodedSecret = encode64($scope.secret);
+							base64EncodedSecret = $scope.base64EncodedSecret = $.base64.encode($scope.secret);
 							$scope.secretUrl = $scope.url_secret_service + encodeURIComponent($scope.secretName);
 							
 							storeSecret($scope.secretUrl, base64EncodedSecret, i);
@@ -2867,77 +2867,6 @@ var rest = myApp.controller(
   			  }	else {
   				formAddPrcsr.sshkeyconfirmpassphrase.style.backgroundColor = '#FA787E';
   			  }
-               
            }
-
-
-		
-		function encode64(input) {
-
-		   input = escape(input);
-		   var output = "";
-		   var chr1, chr2, chr3 = "";
-		   var enc1, enc2, enc3, enc4 = "";
-		   var i = 0;
-		   do {
-			  chr1 = input.charCodeAt(i++);
-			  chr2 = input.charCodeAt(i++);
-			  chr3 = input.charCodeAt(i++);
-			  enc1 = chr1 >> 2;
-			  enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			  enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			  enc4 = chr3 & 63;
-			  if (isNaN(chr2)) {
-				 enc3 = enc4 = 64;
-			  } else if (isNaN(chr3)) {
-				 enc4 = 64;
-			  }
-			  output = output +
-				 keyStr.charAt(enc1) +
-				 keyStr.charAt(enc2) +
-				 keyStr.charAt(enc3) +
-				 keyStr.charAt(enc4);
-			  chr1 = chr2 = chr3 = "";
-			  enc1 = enc2 = enc3 = enc4 = "";
-		   } while (i < input.length);
-		   return output;
-		}
-
-
-
-		function decode64(input) {
-		   var output = "";
-		   var chr1, chr2, chr3 = "";
-		   var enc1, enc2, enc3, enc4 = "";
-		   var i = 0;
-		   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-		   var base64test = /[^A-Za-z0-9\+\/\=]/g;
-		   if (base64test.exec(input)) {
-			  alert("There were invalid base64 characters in the input text.\n" +
-					"Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-					"Expect errors in decoding.");
-		   }
-		   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-		   do {
-			  enc1 = keyStr.indexOf(input.charAt(i++));
-			  enc2 = keyStr.indexOf(input.charAt(i++));
-			  enc3 = keyStr.indexOf(input.charAt(i++));
-			  enc4 = keyStr.indexOf(input.charAt(i++));
-			  chr1 = (enc1 << 2) | (enc2 >> 4);
-			  chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-				chr3 = ((enc3 & 3) << 6) | enc4;
-				output = output + String.fromCharCode(chr1);
-				if (enc3 != 64) {
-				   output = output + String.fromCharCode(chr2);
-				}
-				if (enc4 != 64) {
-				   output = output + String.fromCharCode(chr3);
-				}
-				chr1 = chr2 = chr3 = "";
-
-				enc1 = enc2 = enc3 = enc4 = "";
-			 } while (i < input.length);
-			 return unescape(output);
-		  }
         }
     ]);
