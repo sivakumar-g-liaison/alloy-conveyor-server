f5a160ed84dd256cfd5c27bc16120765049d00c6
jfranklin-ross <jfranklin-ross@liaison.com>
2014-07-24 17:17:23 -0700

    core queue update

--- service-implementation/src/main/java/com/liaison/mailbox/com/liaison/queue/SweeperQueue.java
+++ service-implementation/src/main/java/com/liaison/mailbox/com/liaison/queue/SweeperQueue.java
@@ -1,12 +1,12 @@
 package com.liaison.mailbox.com.liaison.queue;
 
 
-import com.liaison.commons.messagbus.jms.HornetQRecoveringClientPool;
+import com.liaison.commons.messagbus.jms.HornetQRecoveringCoreClientPool;
 
 /**
  * Created by jeremyfranklin-ross on 7/17/14.
  */
-public class SweeperQueue extends HornetQRecoveringClientPool {
+public class SweeperQueue extends HornetQRecoveringCoreClientPool {
 
     public static final String QUEUE_NAME = "sweeper";
     private static SweeperQueue ourInstance = new SweeperQueue();
