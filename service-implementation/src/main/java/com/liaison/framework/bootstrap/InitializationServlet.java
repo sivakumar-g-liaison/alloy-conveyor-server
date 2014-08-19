daf689ba24549316c7a7facdc0a383acc1590edd
Kirithiga Devarajan <kdevarajan@liaison.dev>
2014-08-19 13:27:45 +0530

    All initialization process moved to initialization servlet

--- service-implementation/src/main/java/com/liaison/framework/bootstrap/InitializationServlet.java
+++ service-implementation/src/main/java/com/liaison/framework/bootstrap/InitializationServlet.java
@@ -21,8 +21,16 @@ import javax.servlet.http.HttpServlet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.liaison.commons.acl.util.ACLUtil;
+import com.liaison.commons.acl.util.RemoteURLPublicKeyVerifier;
+import com.liaison.commons.acl.util.SignatureVerifier;
+import com.liaison.commons.acl.util.example.ExampleBase64EncodedSignatureVerifier;
 import com.liaison.commons.audit.AuditStatement.Status;
 import com.liaison.commons.audit.DefaultAuditStatement;
+import com.liaison.commons.jpa.DAOUtil;
+import com.liaison.commons.util.UUIDGen;
+import com.liaison.commons.util.settings.DecryptableConfiguration;
+import com.liaison.commons.util.settings.LiaisonConfigurationFactory;
 import com.liaison.mailbox.com.liaison.queue.ProcessorQueuePoller;
 
 
@@ -43,11 +51,33 @@ public class InitializationServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -8418412083748649428L;
 	private static final Logger logger = LogManager.getLogger(InitializationServlet.class);
+	
+	DecryptableConfiguration configuration = LiaisonConfigurationFactory.getConfiguration();
+    public static final String PROPERTY_USE_MOCK_ACL_SIGNATURE_VERIFIER = "com.liaison.acl.use.mock.verifier";
 
 
     public void init(ServletConfig config) throws ServletException {
+        
         ProcessorQueuePoller.startPolling();
-    	logger.info(new DefaultAuditStatement(Status.SUCCEED,"initialize", com.liaison.commons.audit.pci.PCIV20Requirement.PCI10_2_6));
+      //OracleDataSource.initOracleDataSource(); // TODO
+        DAOUtil.init(); // TODO This does the work of loading all JAP entity files. We
+            // should change to allow the query string to be passed.
+        UUIDGen.init();
+        
+      //Set ACL Filter Signature Verifier
+      SignatureVerifier aclSignatureVerifier; 
+      if (configuration.getBoolean(PROPERTY_USE_MOCK_ACL_SIGNATURE_VERIFIER, false)){
+          aclSignatureVerifier = new ExampleBase64EncodedSignatureVerifier();
+          logger.warn("###############################################################################################################");
+          logger.warn("#### Signature Verifier still set to hardcoded dev example, please update to proper signature verifier /n#### that loads public keys from key manager. EEEEEK! Pay attention to this!");
+          logger.warn("###############################################################################################################");
+      } else {
+          aclSignatureVerifier = new RemoteURLPublicKeyVerifier();
+      }
+      
+      ACLUtil.setSignatureVerifier(aclSignatureVerifier);
+      logger.info(new DefaultAuditStatement(Status.SUCCEED, "ACL Filter Signature Verifier Set: " + aclSignatureVerifier.getClass().getName(), com.liaison.commons.audit.pci.PCIV20Requirement.PCI10_2_6));
+      logger.info(new DefaultAuditStatement(Status.SUCCEED,"initialize via InitializationServlet", com.liaison.commons.audit.pci.PCIV20Requirement.PCI10_2_6));
     }
 
 }
