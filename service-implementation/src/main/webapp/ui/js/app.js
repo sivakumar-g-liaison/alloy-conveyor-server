06da0e4cca20ed46f096ef2dc12e1bbd45e647ec
Veerasamy Nagarajan <vnagarajan@liaison.com>
2014-09-08 15:59:12 -0400

    modified the stored secret url

--- service-implementation/src/main/webapp/ui/js/app.js
+++ service-implementation/src/main/webapp/ui/js/app.js
@@ -89,8 +89,8 @@ myApp.run(function ($rootScope, $location, $http, $timeout, AuthService, RESTSer
     $rootScope.url_ssh_upload_key = $rootScope.kms_base_url+'/upload/keypair';
     $rootScope.url_link_key_store = $rootScope.kms_base_url+'/update/truststore/';
 	//$rootScope.url_secret_service = $rootScope.kms_base_url+'/secret/';
-    $rootScope.url_secret_service = 'http://localhost:3030/key-management/secret/';
-	
+    $rootScope.url_secret_service = 'http://10.0.24.129:8989/key-management/secret/';
+    
 	$rootScope.block = $blockUI.createBlockUI();
 	
     $rootScope.restService = RESTService;
