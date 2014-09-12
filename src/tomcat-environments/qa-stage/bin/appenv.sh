287a6ccc0bdeb30d293f184c2b1b7a354ca5fccb
jfranklin-ross <jfranklin-ross@liaison.com>
2014-09-12 10:04:04 -0700

    appenv for QA log4j for QA

--- src/tomcat-environments/qa-stage/bin/appenv.sh
+++ src/tomcat-environments/qa-stage/bin/appenv.sh
@@ -3,5 +3,12 @@
 # Set application specific system properties in this script
 # ==================================================================
 
-export CATALINA_OPTS="$CATALINA_OPTS -Darchaius.deployment.applicationId=g2mailboxservice"
-export CATALINA_OPTS="$CATALINA_OPTS -Darchaius.deployment.environment=qa-stage"
+export APP_ENV="qa-stage"
+export CATALINA_OPTS="$CATALINA_OPTS -Darchaius.deployment.environment=$APP_ENV"
+export CATALINA_OPTS="$CATALINA_OPTS -Darchaius.deployment.applicationId=service-broker"
+
+# ==================================================================
+# log overrides
+# ==================================================================
+
+export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j.configurationFile=log4j2-$APP_ENV.xml"
