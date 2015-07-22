95ceb2ffb2ddcb9a036410cd167d030a6de2a3b2
Veerasamy <vnagarajan@liaison.com>
2015-07-22 11:28:04 -0400

    GMB-524 - typo

--- service-implementation/src/main/java/com/liaison/mailbox/enums/Messages.java
+++ service-implementation/src/main/java/com/liaison/mailbox/enums/Messages.java
@@ -154,7 +154,7 @@ public enum Messages {
 	COMMON_SYNC_ERROR_MESSAGE("An error condition has been detected within the system. If this condition persists, please contact your administrator or customer support for more details."),
 
 	PAYLOAD_ALREADY_EXISTS("Failed to persist the payload in spectrum because it already exists."),
-	PAYLOAD_PERSIST_ERROR("Failed to persist the payload in spectrum because it already exists."),
+	PAYLOAD_PERSIST_ERROR("Failed to persist the payload in spectrum due to error."),
 	PAYLOAD_READ_ERROR("Failed to retrieve payload from spectrum due to error");
 
 	private String value;
