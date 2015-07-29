7083b305cab92add4c70b80bbc4d7ced1838603b
Kirithiga Devarajan <kdevarajan@liaison.com>
2015-07-29 15:47:28 +0530

    GMB-472 fix applied back after testing

--- service-implementation/src/main/webapp/ui/js/app.js
+++ service-implementation/src/main/webapp/ui/js/app.js
@@ -34,12 +34,12 @@ myApp.config(['$routeProvider', '$locationProvider', '$httpProvider',
         //$locationProvider.html5Mode(true);
 		
 		//GMB-472 Fix - Disable $http request cache
-		/*$httpProvider.defaults.cache = false;
+		$httpProvider.defaults.cache = false;
 		if (!$httpProvider.defaults.headers.common) {
 	        $httpProvider.defaults.headers.common = {};
 	    }
 	    $httpProvider.defaults.headers.common["Cache-Control"] = "no-cache";
-	    $httpProvider.defaults.headers.common.Pragma = "no-cache";*/
+	    $httpProvider.defaults.headers.common.Pragma = "no-cache";
     
         $routeProvider.when('/', {
             templateUrl: 'partials/home.html'
