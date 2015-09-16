be64af2a16d190345ad215838822cb6b440d17b0
Veerasamy Nagarajan <vnagarajan@liaison.com>
2015-09-16 15:59:01 -0400

    GMB-602 - Customer pick up LENS status update

---
+++ service-implementation/src/test/java/com/liaison/mailbox/service/integration/test/MailBoxWatchDogServiceIT.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright Liaison Technologies, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Liaison Technologies, Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Liaison Technologies.
+ */
+
+package com.liaison.mailbox.service.integration.test;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.liaison.mailbox.service.base.test.BaseServiceTest;
+import com.liaison.mailbox.service.base.test.InitInitialDualDBContext;
+import com.liaison.mailbox.service.core.sla.MailboxWatchDogService;
+
+/**
+ * Test class to test mailbox watchdog service.
+ * 
+ * @author veerasamyn
+ */
+public class MailBoxWatchDogServiceIT extends BaseServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeMethod
+	public void setUp() throws Exception {
+		System.setProperty("com.liaison.secure.properties.path", "invalid");
+		System.setProperty("archaius.deployment.applicationId", "g2mailboxservice");
+		System.setProperty("archaius.deployment.environment", "test");
+		InitInitialDualDBContext.init();
+	}
+
+	/**
+	 * This is just to check the flow and DB operations. Not intened to test full functionality
+	 * 
+	 */
+	@Test(enabled=false)
+	public void testCreateMailBox() {
+		MailboxWatchDogService service = new MailboxWatchDogService();
+		service.pollAndUpdateStatus();
+	}
+}
