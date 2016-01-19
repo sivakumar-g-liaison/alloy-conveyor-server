7415a4042b9b21a5580043b3fafe17c18497f6c1
Veerasamy Nagarajan <vnagarajan@liaison.com>
2016-01-19 12:03:43 -0500

    license header upgraded to 1.10.0 and added license header for missing classes

--- service-implementation/src/test/java/com/liaison/mailbox/service/integration/test/ProfileConfigurationServiceIT.java
+++ service-implementation/src/test/java/com/liaison/mailbox/service/integration/test/ProfileConfigurationServiceIT.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2016 Liaison Technologies, Inc.
+ * This software is the confidential and proprietary information of
+ * Liaison Technologies, Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Liaison Technologies.
+ */
 package com.liaison.mailbox.service.integration.test;
 
 import java.util.ArrayList;
