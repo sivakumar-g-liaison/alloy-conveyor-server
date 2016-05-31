5fa494568a9a5cea6b2f3a4298f710a3f0f0a437
Kirithiga Devarajan <kdevarajan@liaison.com>
2016-05-31 19:35:03 +0530

    GMB-790 Allow the sweeper to have a property that will allow it to sweep subdirectories

--- service-implementation/src/test/java/com/liaison/mailbox/service/integration/test/DirectorySweeperTest.java
+++ service-implementation/src/test/java/com/liaison/mailbox/service/integration/test/DirectorySweeperTest.java
@@ -9,38 +9,37 @@
  */
 package com.liaison.mailbox.service.integration.test;
 
+import com.liaison.commons.jaxb.JAXBUtility;
+import com.liaison.commons.util.UUIDGen;
+import com.liaison.dto.queue.WorkTicket;
+import com.liaison.framework.util.ServiceUtils;
+import com.liaison.mailbox.dtdm.model.Folder;
+import com.liaison.mailbox.dtdm.model.MailBox;
+import com.liaison.mailbox.dtdm.model.Processor;
+import com.liaison.mailbox.dtdm.model.Sweeper;
+import com.liaison.mailbox.service.base.test.BaseServiceTest;
+import com.liaison.mailbox.service.core.processor.DirectorySweeper;
+import com.liaison.mailbox.service.dto.configuration.processor.properties.SweeperPropertiesDTO;
+import com.liaison.mailbox.service.util.ProcessorPropertyJsonMapper;
+import org.codehaus.jettison.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import javax.xml.bind.JAXBException;
-
-import org.codehaus.jettison.json.JSONException;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import com.liaison.framework.util.ServiceUtils;
-import com.liaison.fs2.api.exceptions.FS2Exception;
-import com.liaison.mailbox.dtdm.model.Folder;
-import com.liaison.mailbox.dtdm.model.Processor;
-import com.liaison.mailbox.service.base.test.BaseServiceTest;
-import com.liaison.mailbox.service.core.processor.DirectorySweeper;
-import com.liaison.mailbox.service.exception.MailBoxServicesException;
-import com.liaison.mailbox.service.util.MailBoxUtil;
 
 /**
  * @author OFS
  *
  */
-@Test(enabled = false)
+@Test
 public class DirectorySweeperTest extends BaseServiceTest {
 
 	private String inbox;
@@ -51,8 +50,9 @@ public class DirectorySweeperTest extends BaseServiceTest {
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@BeforeMethod
+	@BeforeClass
 	public void setUp() throws Exception {
+
 		inbox = System.getProperty("java.io.tmpdir") + File.separator + "INBOX1";
 		Files.deleteIfExists(Paths.get(inbox));
 		Files.createDirectory(Paths.get(inbox));
@@ -61,182 +61,107 @@ public class DirectorySweeperTest extends BaseServiceTest {
 		excludeFiles = ".js";
 	}
 
+    @AfterClass
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(Paths.get(inbox));
+    }
+
 	/**
 	 * Method to test Sweeper with valid data.
 	 *
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 * @throws MailBoxServicesException
-	 * @throws FS2Exception
-	 * @throws JAXBException
-	 * @throws IllegalAccessException
-	 * @throws IllegalArgumentException
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
+	 * @throws Exception
 	 */
-    @Test(enabled = false)
-	public void testSweeper() throws IOException, URISyntaxException, MailBoxServicesException, FS2Exception, JAXBException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    @Test(enabled = true)
+	public void testSweeper() throws Exception {
 
 		String name = "Purchase.txt";
 		Path target = Paths.get(inbox + File.separator + name);
 		Files.createFile(target);
+        Thread.sleep(2000);
 
-		Processor processor = new Processor();
-		processor.setProcsrName("Processor");
-		processor.setProcsrStatus("ACTIVE");
-		String remoteProperties = ServiceUtils.readFileFromClassPath("requests/processor/remoteprocessor.json");
-		processor.setProcsrProperties(remoteProperties);
+        try {
 
-		Set<Folder> folders = new HashSet<Folder>();
-		Folder folder = new Folder();
-		folder.setFldrType("PAYLOAD_LOCATION");
-		folder.setFldrUri(inbox);
-		folders.add(folder);
-		processor.setFolders(folders);
+            List<WorkTicket> workTickets = sweep();
+            Assert.assertEquals(workTickets.get(0).getFileName(), name);
+        } finally {
+            Files.deleteIfExists(target);
+        }
 
-		DirectorySweeper downloader = new DirectorySweeper(processor);
-		List<String> includeList = (!MailBoxUtil.isEmpty(includeFiles))? Arrays.asList(includeFiles.split(",")) : null;
-		List<String> excludeList = (!MailBoxUtil.isEmpty(excludeFiles)) ? Arrays.asList(excludeFiles.split(",")) : null;
-		//List<WorkTicket> path = downloader.sweepDirectory(inbox, false, fileRenameFormat, 0, includeList, excludeList);
-		Files.delete(target);
-		//Assert.assertEquals(1, path.size());
-		//Assert.assertEquals(name, path.get(0).getFileName());
 	}
 
-	/**
+    /**
 	 * Method to test Sweeper without file permission.
 	 *
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 * @throws MailBoxServicesException
-	 * @throws FS2Exception
 	 * @throws Exception
 	 */
-    @Test(enabled = false)
-	public void testSweeperWithoutFilePermission() throws IOException, URISyntaxException, MailBoxServicesException,
-			FS2Exception, Exception {
+    @Test(enabled = true)
+	public void testSweeperWithoutFilePermission() throws Exception {
 
 		String name = "PurchaseError.txt";
 		Path target = Paths.get(inbox + File.separator + name);
 		Files.createFile(target);
 		target.toFile().setReadable(false);
 		target.toFile().setWritable(false);
+        target.toFile().setReadOnly();
+        Thread.sleep(5000);
 
-		Processor processor = new Processor();
-		processor.setProcsrName("Processor");
-		processor.setProcsrStatus("ACTIVE");
-		String remoteProperties = ServiceUtils.readFileFromClassPath("requests/processor/remoteprocessor.json");
-		processor.setProcsrProperties(remoteProperties);
-
-		Set<Folder> folders = new HashSet<Folder>();
-		Folder folder = new Folder();
-		folder.setFldrType("PAYLOAD_LOCATION");
-		folder.setFldrUri(inbox);
-		folders.add(folder);
-		processor.setFolders(folders);
-
-		DirectorySweeper downloader = new DirectorySweeper(processor);
-		List<String> includeList = (!MailBoxUtil.isEmpty(includeFiles))? Arrays.asList(includeFiles.split(",")) : null;
-		List<String> excludeList = (!MailBoxUtil.isEmpty(excludeFiles)) ? Arrays.asList(excludeFiles.split(",")) : null;
-		//List<WorkTicket> path = downloader.sweepDirectory(inbox, false, fileRenameFormat, 0, includeList, excludeList);
-		target.toFile().setReadable(true);
-		target.toFile().setWritable(true);
-		Files.delete(target);
-		//Assert.assertEquals(1, path.size());
-		//Assert.assertEquals(name, path.get(0).getFileName());
+        try {
+            List<WorkTicket> workTickets = sweep();
+            Assert.assertEquals(workTickets.get(0).getFileName(), name);
+        } finally {
+            target.toFile().setReadable(true);
+            target.toFile().setWritable(true);
+            Files.delete(target);
+        }
 	}
 
 	/**
 	 * Method to test Sweeper with space in file name.
 	 *
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 * @throws MailBoxServicesException
-	 * @throws FS2Exception
-	 * @throws JAXBException
-	 * @throws IllegalAccessException
-	 * @throws IllegalArgumentException
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
+	 * @throws Exception
 	 */
-    @Test(enabled = false)
-	public void testSweeperWithSpaceInFileName() throws IOException, URISyntaxException, MailBoxServicesException, FS2Exception,
-			JAXBException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    @Test(enabled = true)
+	public void testSweeperWithSpaceInFileName() throws Exception {
 
 		String name = "Purchase Error.txt";
 		Path target = Paths.get(inbox + File.separator + name);
 		Files.createFile(target);
+        Thread.sleep(5000);
 
-		Processor processor = new Processor();
-		processor.setProcsrName("Processor");
-		processor.setProcsrStatus("ACTIVE");
-		String remoteProperties = ServiceUtils.readFileFromClassPath("requests/processor/remoteprocessor.json");
-		processor.setProcsrProperties(remoteProperties);
-
-		Set<Folder> folders = new HashSet<Folder>();
-		Folder folder = new Folder();
-		folder.setFldrType("PAYLOAD_LOCATION");
-		folder.setFldrUri(inbox);
-		folders.add(folder);
-		processor.setFolders(folders);
-
-		DirectorySweeper downloader = new DirectorySweeper(processor);
-		List<String> includeList = (!MailBoxUtil.isEmpty(includeFiles))? Arrays.asList(includeFiles.split(",")) : null;
-		List<String> excludeList = (!MailBoxUtil.isEmpty(excludeFiles)) ? Arrays.asList(excludeFiles.split(",")) : null;
-		//List<WorkTicket> path = downloader.sweepDirectory(inbox, false, fileRenameFormat, 0, includeList, excludeList);
-		Files.delete(target);
-		//Assert.assertEquals(1, path.size());
-		//Assert.assertEquals(name, path.get(0).getFileName());
+        try {
+            List<WorkTicket> workTickets = sweep();
+            Assert.assertEquals(workTickets.get(0).getFileName(), name);
+        } finally {
+            Files.delete(target);
+        }
 	}
 
-	/**
-	 * Method to test mark as sweeped.
-	 *
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 * @throws MailBoxServicesException
-	 * @throws FS2Exception
-	 * @throws JSONException
-	 * @throws JAXBException
-	 * @throws IllegalAccessException
-	 * @throws IllegalArgumentException
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
-	 */
-    @Test(enabled = false)
-	public void testMarkAsSweeped() throws IOException, URISyntaxException, MailBoxServicesException, FS2Exception,
-			JSONException, JAXBException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    private List<WorkTicket> sweep() throws Exception {
 
-		String name = "Purchase Error.txt";
-		Path target = Paths.get(inbox + File.separator + name);
-		Files.createFile(target);
+        Processor processor = new Sweeper();
+        processor.setProcsrName("Processor");
+        processor.setProcsrStatus("ACTIVE");
+        processor.setProcsrProperties(ServiceUtils.readFileFromClassPath("requests/processor/sweeper.json"));
 
-		Processor processor = new Processor();
-		processor.setProcsrName("Processor");
-		processor.setProcsrStatus("ACTIVE");
-		String remoteProperties = ServiceUtils.readFileFromClassPath("requests/processor/remoteprocessor.json");
-		processor.setProcsrProperties(remoteProperties);
+        MailBox mailbox = new MailBox();
+        mailbox.setMbxName("Test");
+        mailbox.setPguid(UUIDGen.getCustomUUID());
+        processor.setMailbox(mailbox);
 
-		Set<Folder> folders = new HashSet<Folder>();
-		Folder folder = new Folder();
-		folder.setFldrType("PAYLOAD_LOCATION");
-		folder.setFldrUri(inbox);
-		folders.add(folder);
-		processor.setFolders(folders);
+        Set<Folder> folders = new HashSet<Folder>();
+        Folder folder = new Folder();
+        folder.setFldrType("PAYLOAD_LOCATION");
+        folder.setFldrUri(inbox);
+        folders.add(folder);
+        processor.setFolders(folders);
 
-		DirectorySweeper downloader = new DirectorySweeper(processor);
-		List<String> includeList = (!MailBoxUtil.isEmpty(includeFiles))? Arrays.asList(includeFiles.split(",")) : null;
-		List<String> excludeList = (!MailBoxUtil.isEmpty(excludeFiles)) ? Arrays.asList(excludeFiles.split(",")) : null;
-		//List<WorkTicket> files = downloader.sweepDirectory(inbox, false, fileRenameFormat, 0, includeList, excludeList);
-		Files.delete(target);
-		//Assert.assertEquals(1, files.size());
-		//Assert.assertEquals(name, files.get(0).getFileName());
+        DirectorySweeper sweeper = new DirectorySweeper(processor);
+        SweeperPropertiesDTO staticProp = (SweeperPropertiesDTO) ProcessorPropertyJsonMapper.getProcessorBasedStaticPropsFromJson
+                (processor.getProcsrProperties(), processor);
+        List<WorkTicket> workTickets = sweeper.sweepDirectory(inbox, staticProp);
 
-	}
-
-	@AfterMethod
-	public void tearDown() throws Exception {
-		Files.deleteIfExists(Paths.get(inbox));
-	}
+        System.out.println(new JSONObject(JAXBUtility.marshalToJSON(workTickets.get(0))).toString(2));
+        return workTickets;
+    }
 
 }
