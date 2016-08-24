de46297fe17ef3374a961f8f115f3ce604e825e2
Veerasamy Nagarajan <vnagarajan@liaison.com>
2016-08-24 11:05:18 -0400

    added copyright

--- service-implementation/src/test/resources/scripts/sftp_uploader.js
+++ service-implementation/src/test/resources/scripts/sftp_uploader.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2016 Liaison Technologies, Inc.
+ * This software is the confidential and proprietary information of
+ * Liaison Technologies, Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Liaison Technologies.
+ */
+
 /**
  * Created by VNagarajan on 7/20/2016.
  */
