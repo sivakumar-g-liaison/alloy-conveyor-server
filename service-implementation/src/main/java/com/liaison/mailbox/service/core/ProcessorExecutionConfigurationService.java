8a6477fd46ed369d906dc79fd4e3364df0992203
Kirithiga Devarajan <kdevarajan@liaison.com>
2017-03-24 19:19:03 +0530

    GMB-970 Rest service to update multiple processor guid status

--- service-implementation/src/main/java/com/liaison/mailbox/service/core/ProcessorExecutionConfigurationService.java
+++ service-implementation/src/main/java/com/liaison/mailbox/service/core/ProcessorExecutionConfigurationService.java
@@ -26,16 +26,19 @@ import com.liaison.mailbox.service.dto.ResponseDTO;
 import com.liaison.mailbox.service.dto.configuration.response.ExecutingProcessorsDTO;
 import com.liaison.mailbox.service.dto.configuration.response.GetProcessorExecutionStateResponseDTO;
 import com.liaison.mailbox.service.dto.configuration.response.UpdateProcessorExecutionStateResponseDTO;
+import com.liaison.mailbox.service.dto.configuration.response.UpdateProcessorsExecutionStateResponseDTO;
 import com.liaison.mailbox.service.exception.MailBoxConfigurationServicesException;
 import com.liaison.mailbox.service.topic.TopicMessageDTO;
 import com.liaison.mailbox.service.topic.producer.MailBoxTopicMessageProducer;
 import com.liaison.mailbox.service.util.MailBoxUtil;
 import com.netflix.config.ConfigurationManager;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBException;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -325,8 +328,39 @@ public class ProcessorExecutionConfigurationService {
      * 
      */
     public static void updateExecutionStateOnInit() {
+        
         ProcessorExecutionStateDAO processorExecutionStateDAO = new ProcessorExecutionStateDAOBase();
         processorExecutionStateDAO.updateStuckProcessorsExecutionState(ConfigurationManager.getDeploymentContext().getDeploymentServerId());
     }
-
+    
+    /**
+     * Method to update the state of executing processors to failed.
+     * 
+     * @param processorIds
+     * @param userId
+     * @return response
+     */
+    public UpdateProcessorsExecutionStateResponseDTO updateExecutingProcessors(List<String> processorIds, String userId) { 
+        
+        UpdateProcessorsExecutionStateResponseDTO response = new UpdateProcessorsExecutionStateResponseDTO();
+        
+            try {
+                
+                for (String processorId : processorIds) {
+                
+                    if (StringUtil.isNullOrEmptyAfterTrim(processorId)) {
+                        throw new MailBoxConfigurationServicesException(Messages.PROCESSOR_ID_NOT_AVAILABLE, Response.Status.BAD_REQUEST);
+                    }
+                    this.interruptAndUpdateStatus(processorId, userId, null, null);
+                }
+                response.setResponse(new ResponseDTO(Messages.REVISED_SUCCESSFULLY,
+                    "The processor execution status for processorIds updated.", Messages.SUCCESS));
+                return response;
+            } catch (MailBoxConfigurationServicesException e) {
+                
+                LOG.error(Messages.REVISE_OPERATION_FAILED.name(), e);
+                response.setResponse(new ResponseDTO(Messages.REVISE_OPERATION_FAILED, PROCESSORS, Messages.FAILURE, e.getMessage()));
+                return response;
+            }
+    }
 }
