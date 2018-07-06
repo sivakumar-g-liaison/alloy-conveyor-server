d0b8214cef9801b917f2324ac37ed8ef1270f289
Veerasamy Nagarajan <vnagarajan@liaison.com>
2018-07-06 14:06:48 -0400

    GMB-1130 - Write permissions when using File Writer to create new directories and write a file the first time.

--- service-implementation/src/main/resources/g2mailboxservice-dev-int-at4.sh
+++ service-implementation/src/main/resources/g2mailboxservice-dev-int-at4.sh
@@ -4,4 +4,4 @@
 # ==================================================================
 #Enabled Remote Debugging
 export CATALINA_OPTS="$CATALINA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-echo "loading g2mailboxservice-dev-int-at4-dev-pres.sh"
+echo "loading g2mailboxservice-dev-int-at4.sh"
