a7738062a9c9c219de662abad82d20eecddc7024
Janne Keränen <janne.keranen@domain.anilinker.com>
2019-03-18 13:44:20 +0200

    updated license headers

--- service-framework/src/main/java/com/liaison/framework/exceptions/UnhandledApplicationException.java
+++ service-framework/src/main/java/com/liaison/framework/exceptions/UnhandledApplicationException.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2019 Liaison Technologies, Inc.
+ * This software is the confidential and proprietary information of
+ * Liaison Technologies, Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Liaison Technologies.
+ */
 package com.liaison.framework.exceptions;
 
 import com.liaison.commons.audit.AuditStandardsRequirement;
