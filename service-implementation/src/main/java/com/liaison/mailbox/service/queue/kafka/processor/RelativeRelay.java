21484d82d7e0d200c73f5fea24340dc823d4d1de
Sivakumar Gopalakrishnan <Gopalakrishnan@liaison.com>
2019-05-27 14:51:22 +0530

    Updated related relay processor license

--- service-implementation/src/main/java/com/liaison/mailbox/service/queue/kafka/processor/RelativeRelay.java
+++ service-implementation/src/main/java/com/liaison/mailbox/service/queue/kafka/processor/RelativeRelay.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright Liaison Technologies, Inc. All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * Liaison Technologies, Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Liaison Technologies.
+ */
 package com.liaison.mailbox.service.queue.kafka.processor;
 
 import static java.lang.annotation.ElementType.FIELD;
