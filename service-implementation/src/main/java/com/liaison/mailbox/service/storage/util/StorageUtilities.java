e900b829d717dcaff28580dffbfd6c2c826f5c60
Sivakumar Gopalakrishnan <Gopalakrishnan@liaison.com>
2019-06-03 16:42:07 +0530

    Throw exception to test retry for sweeper event

--- service-implementation/src/main/java/com/liaison/mailbox/service/storage/util/StorageUtilities.java
+++ service-implementation/src/main/java/com/liaison/mailbox/service/storage/util/StorageUtilities.java
@@ -233,17 +233,18 @@ public class StorageUtilities {
             // fetch the metdata includes payload size
             FS2MetaSnapshot metaSnapshot;
             try {
-                metaSnapshot = FS2.createObjectEntry(requestUri, generateFS2Options(workTicket), fs2Header, payload);
-                LOGGER.debug("Time spent on uploading file {} of size {} to fs2 storage only is {} ms",
-                        workTicket.getFileName(), metaSnapshot.getPayloadSize(), endTime - startTime);
+            	throw new FS2Exception("Test retry process");
+//                metaSnapshot = FS2.createObjectEntry(requestUri, generateFS2Options(workTicket), fs2Header, payload);
+//                LOGGER.debug("Time spent on uploading file {} of size {} to fs2 storage only is {} ms",
+//                        workTicket.getFileName(), metaSnapshot.getPayloadSize(), endTime - startTime);
             } finally {
                 if (payload != null) {
                     payload.close();
                 }
             }
 
-            LOGGER.debug("Successfully persist the payload in fs2 storage to url {} ", requestUri);
-            return metaSnapshot;
+//            LOGGER.debug("Successfully persist the payload in fs2 storage to url {} ", requestUri);
+//            return metaSnapshot;
 
         } catch (FS2ObjectAlreadyExistsException e) {
             LOGGER.error(Messages.PAYLOAD_ALREADY_EXISTS.value(), e);
