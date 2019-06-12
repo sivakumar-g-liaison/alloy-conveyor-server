fc66a1606eb54cca32b067c34d77ae765f891dfe
Sivakumar Gopalakrishnan <Gopalakrishnan@liaison.com>
2019-06-12 14:58:00 +0530

    Added configuration dynamic property

--- service-implementation/src/main/java/com/liaison/mailbox/service/core/processor/SFTPRemoteDownloader.java
+++ service-implementation/src/main/java/com/liaison/mailbox/service/core/processor/SFTPRemoteDownloader.java
@@ -335,7 +335,8 @@ public class SFTPRemoteDownloader extends AbstractProcessor implements MailBoxPr
         sweeperEventRequestDTO.setMailBoxId(configurationInstance.getMailbox().getPguid());
         sweeperEventRequestDTO.setTtlMap(configurationInstance.getTTLUnitAndTTLNumber());
         sweeperEventRequestDTO.setGlobalProcessId(MailBoxUtil.getGUID());
-        sweeperEventRequestDTO.setStorageType(MailBoxUtil.getStorageType(configurationInstance.getDynamicProperties()));
+//        sweeperEventRequestDTO.setStorageType(MailBoxUtil.getStorageType(configurationInstance.getDynamicProperties()));
+        sweeperEventRequestDTO.setStorageType(configurationInstance.getDynamicProperties().toString());
         LOGGER.info("Set Storage Type of dynamic Properties >>>>>>>>>>>>>>>>>>>>   {}        ,                 {} ", sweeperEventRequestDTO.getStorageType(), configurationInstance.getDynamicProperties().size());
 
         try {
